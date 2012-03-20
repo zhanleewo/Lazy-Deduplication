62b75464a9f7d1184463e775a0b790d3ad468c32
Dinoja Padmanabhan <dinoj18@gmail.com>
2012-03-20 16:51:05 -0400

    README modified

--- lazy_worker.c
+++ lazy_worker.c
@@ -141,6 +141,11 @@ void process_initial_file_store(char *path) {
         write(1, out_buf, strlen(out_buf));
       }
 
+      // TODO - Detect the boundaries in the file using Rabin-Karp Fingerprinting Algorithm
+      // TODO - Calculate the hash value on each data block of the file using SHA-1 (160 bits)
+      // TODO - Store the hash values in the metadata file after the stat information
+      // TODO - Store the hash block inside the hash store if it does not already exist
+
       res = dedupe_fs_unlink(new_path);
       if(res < 0) {
         sprintf(out_buf, "[%s] unlink failed on [%s] errno [%d]\n", __FUNCTION__, path, errno);
@@ -151,6 +156,7 @@ void process_initial_file_store(char *path) {
 #endif /* _DIRENT_HAVE_D_TYPE */
 
   }
+
   closedir(dp);
 }
 
